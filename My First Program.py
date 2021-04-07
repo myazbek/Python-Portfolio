2993514545d613f5dfaaeadf302de7d86bfefb14
myazbek <81050631+myazbek@users.noreply.github.com>
2021-04-07 16:52:05 -0700

    Add files via upload

---
+++ My First Program.py
@@ -0,0 +1,13 @@
+new = 2
+read = 1
+print(f"{new - read} new messages")
+messages_total = new + read
+
+print(f"{messages_total} total messages")
+print ("-----------------------")
+name = input("What is your name? ")
+print("-----------------------")
+print(f"Hello {name} :)")
+print("-----------------------")
+print("You look great today!")
+print("-----------------------")
